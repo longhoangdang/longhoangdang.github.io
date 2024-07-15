289aa8aa01cca4b27c145a1c1944c8fa8b6dc8f8
Long Hoang Dang <88375914+longhoangdang@users.noreply.github.com>
2024-07-15 15:13:10 +0700

    Update my_info.js

--- components/my_info.js
+++ components/my_info.js
@@ -6,38 +6,34 @@ class MyInfo extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
 		<div class="row">
-			<img src="assets/img/KienDo.jpg" alt="Kien Do" style="border-radius: 50%; width: 70%; height: auto; float: left;">
+			<img src="assets/img/Profile.png" alt="Long Hoang Dang" style="border-radius: 50%; width: 70%; height: auto; float: left;">
 		</div>
 
 		<div class="row" style="padding-top: 0.6rem">
 			<ul style="list-style-type: none; padding-left: 1rem">
 				<li>
-					Research Fellow<br>
-					A2I2, Deakin University<br>
+					Lecturer<br>
+					Faculty of Information Technology, PTIT<br>
 				</li>
 				<li>
 					<i class="bi bi-envelope-fill" style="font-size: 0.85rem"></i>
-					<a href="mailto:hunken92(at)gmail(dot)com"> Email</a>
-				</li>
-				<li>
-					<i class="bi bi-envelope-fill" style="font-size: 0.85rem"></i>
-					<a href="mailto:k.do(at)deakin(dot)edu(dot)au"> Work Email</a>
+					<a href="mailto:longdh(at)ptit(dot)edu(dot)vn"> Email</a>
 				</li>
 				<li>
 					<i class="ai ai-google-scholar-square"></i>
-					<a href="https://scholar.google.com/citations?user=aD6y8joAAAAJ&hl=en"> Google Scholar</a>
+					<a href="https://scholar.google.co.jp/citations?user=unEG5bgAAAAJ&hl=en"> Google Scholar</a>
 				</li>
 				<li>
 					<i class="bi bi-linkedin" style="font-size: 0.85rem"></i>
-					<a href="https://www.linkedin.com/in/kien-do-b45846a4/"> LinkedIn</a>
+					<a href="https://www.linkedin.com/in/long-hoang-dang-45b58b132/"> LinkedIn</a>
 				</li>
 				<li>
 					<i class="bi bi-twitter" style="font-size: 0.85rem"></i>
-					<a href="https://twitter.com/kien_do_92"> Twitter</a>
+					<a href="https://x.com/hldang96"> Twitter</a>
 				</li>
 				<li>
 					<i class="bi bi-github" style="font-size: 0.85rem"></i>
-					<a href="https://github.com/clarken92"> Github</a>
+					<a href="https://github.com/longhoangdang"> Github</a>
 				</li>
 			</ul>
 		</div>
